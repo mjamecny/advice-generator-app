be9dfb68669a8b36f3d84ce3f367c255f8f54af1
mjamecny <jamecny.m@tutanota.com>
2023-11-01 22:24:52 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react"
+
+const getAdvice = async () => {
+  const res = await fetch("https://api.adviceslip.com/advice")
+  const data = await res.json()
+  return data.slip
+}
+
+export default function App() {
+  const [advice, setAdvice] = useState("")
+  const [adviceId, setAdviceId] = useState(0)
+
+  useEffect(() => {
+    async function handleAdvice() {
+      const obj = await getAdvice()
+      setAdviceId(obj.id)
+      setAdvice(obj.advice)
+    }
+    handleAdvice()
+  }, [])
+
+  async function handleAdvice() {
+    const obj = await getAdvice()
+    setAdviceId(obj.id)
+    setAdvice(obj.advice)
+  }
+
+  return (
+    <div className="container">
+      <div className="advice">
+        <div className="advice-box">
+          <h2 className="advice__title">Advice #{adviceId}</h2>
+          <p className="advice__text">“{advice}”</p>
+          <picture>
+            <source
+              srcset="pattern-divider-mobile.svg"
+              media="(max-width: 34em)"
+            />
+            <img
+              class="advice__divider"
+              src="pattern-divider-desktop.svg"
+              alt="divider"
+            />
+          </picture>
+        </div>
+
+        <div className="advice__action-btn" onClick={handleAdvice}>
+          <img src="icon-dice.svg" alt="dice icon" />
+        </div>
+      </div>
+    </div>
+  )
+}
